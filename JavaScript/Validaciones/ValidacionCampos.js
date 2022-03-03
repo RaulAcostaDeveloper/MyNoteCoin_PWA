5a63b276132ea1713c533e0ea1817637be576cf1
Raúl <raulacostadeveloper@outlook.com>
2022-03-02 18:02:32 -0600

    Validaciones

--- JavaScript/Validaciones/ValidacionCampos.js
+++ JavaScript/Validaciones/ValidacionCampos.js
@@ -11,3 +11,24 @@ function valideKey(evt){
       return false;
     }
  }
+
+ function longitudDiasDePago(evt, PaymentDayId){
+    var code = (evt.which) ? evt.which : evt.keyCode;
+    let diaDePago = document.getElementById(PaymentDayId).value;
+ 
+    if(code==8 || code>=48 && code<=57 && diaDePago.length < 2) { // backspace.
+      return true;
+    } else {
+        return false;
+    }
+ }
+
+ function limpiarForm(ID){
+     console.log('limpiarForm ' + ID);
+    let formulario = document.getElementById(ID);
+    let formElement = formulario.querySelector('form');
+    let inputElement = formElement.querySelectorAll('input');
+    for (let index = 0; index < inputElement.length; index++) {
+        inputElement[index].value = "";
+    }
+}
